08c7b1b1b24c0bbfe9334e921e7e32735eebc99c
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2016-09-25 11:50:28 -0500

    Updating to new imagemin code.

--- reduce.js
+++ reduce.js
@@ -1,8 +1,11 @@
-"use strict";
 var imagemin = require("imagemin"),
 	jpegRecompress = require("imagemin-jpeg-recompress");
 
-new imagemin().src("img/*.jpg").dest("optimg").use(jpegRecompress({
-	accurate: true,
-	max: 70
-})).run();
+imagemin(["img/*.jpg"], "optimg", {
+	plugins: [
+		jpegRecompress({
+			accurate: true,
+			max: 70
+		})
+	]
+});
