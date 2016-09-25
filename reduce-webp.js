dd9c7121e985f8696312a1eb5b7a88d9f017fac1
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2016-09-25 13:05:31 -0500

    Updating WebP image conversion code.

--- reduce-webp.js
+++ reduce-webp.js
@@ -1,7 +1,8 @@
-"use strict";
 var imagemin = require("imagemin"),
 	webp = require("imagemin-webp");
 
-new imagemin().src("img/*.png").dest("optimg").use(webp({
-	lossless: true
-})).run();
+imagemin(["img/*.png"], "optimg", {
+	plugins: [webp({
+		lossless: true
+	})]
+});
